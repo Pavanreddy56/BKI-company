e93011f6f38e5f9b3c555d6bb7c9e70566ef1d23
cheedeti pavan reddy <cheedetipavanreddy@gmail.com>
2025-12-04 10:29:44 +0530

    Create Tracking.tsx

---
+++ client/src/pages/Tracking.tsx
@@ -0,0 +1,8 @@
+export default function Tracking() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Tracking Page</h1>
+      <p>This page is under development.</p>
+    </div>
+  );
+}
